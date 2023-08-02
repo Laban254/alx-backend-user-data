54a72819dcca5c292431f02b6de1df277c34a4d8
laban254 <labanrotich6544@gmail.com>
2023-08-02 22:32:02 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ Regex-ing """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(
@@ -16,3 +17,31 @@ def filter_datum(
         message = re.sub(
                 f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        """
+        Constructor method for RedactingFormatter class
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the specified log record as text.
+        Filters values in incoming log records using filter_datum
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == '__main__':
+        main()
