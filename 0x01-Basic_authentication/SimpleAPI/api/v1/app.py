79897c14d609c9eaf5b901a079ff2b887f661d22
laban254 <labanrotich6544@gmail.com>
2023-08-08 18:39:44 +0000

    Request validation!

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -6,13 +6,13 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
-import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+auth = None
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -30,6 +30,20 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """403 unauthorized handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """before request handler"""
+    authorized = ['/api/v1/status/', '/api/v1/unauthorized/',
+                  '/api/v1/forbidden/']
+
+    if auth and auth.require_auth(request.path, authorized):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+
 
 
 if __name__ == "__main__":
