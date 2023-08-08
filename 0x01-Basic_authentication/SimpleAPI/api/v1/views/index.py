dc477bba56a4fa8b610ae8046ee8e92f95c7fc22
laban254 <labanrotich6544@gmail.com>
2023-08-08 18:27:59 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/V1/unauthorized
+        return aborts"""
+    abort(401)
