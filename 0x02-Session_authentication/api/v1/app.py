a9d10c478a4853dd6f9f87e2a4142f6c68b41ce4
Laban Kibet <64686919+Laban254@users.noreply.github.com>
2023-08-12 12:33:40 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,8 +5,11 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
+from flask_cors import (CORS, cross_origin)
+from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -14,14 +17,19 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
+
 if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
-# elif getenv("AUTH_TYPE") == "basic_auth":
-#     auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
@@ -40,16 +48,19 @@ def forbidden(error) -> str:
 @app.before_request
 def before_request():
     """before request handler"""
-    authorized = ['/api/v1/status/', '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/']
+    authorized_list = ['/api/v1/status/',
+                       '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                       '/api/v1/auth_session/login/']
 
-    if auth and auth.require_auth(request.path, authorized):
+    if auth and auth.require_auth(request.path, authorized_list):
         if not auth.authorization_header(request):
+            abort(401)
+        if (auth.authorization_header(request) and
+                not auth.session_cookie(request)):
             abort(401)
         request.current_user = auth.current_user(request)
         if not auth.current_user(request):
             abort(403)
-
 
 
 if __name__ == "__main__":
