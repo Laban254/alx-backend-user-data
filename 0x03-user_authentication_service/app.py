f692ec3eee37af1c6b1daa1826726ff6005e01e8
laban254 <labanrotich6544@gmail.com>
2023-08-16 20:51:30 +0000

    register user with  using POST

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """flask app """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     response = jsonify({"message": "Bienvenue"})
     return response
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """post  a user"""
+    email = request.form.get("email")
+    password = request.form.get("passwor")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
