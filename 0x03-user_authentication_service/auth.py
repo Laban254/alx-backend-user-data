7a9ed87af456856e231aee84d46b9e6a0a1cadbb
laban254 <labanrotich6544@gmail.com>
2023-08-16 18:02:57 +0000

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Hash password """
+
+import bcrypt
+
+
+def _hash_password(password) -> bytes:
+    """return encrypted password"""
+
+    bytes_pw = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(bytes_pw, salt)
+    return hashed
