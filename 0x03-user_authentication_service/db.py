21af77a3550eb75f952e451555be322b25766201
laban254 <labanrotich6544@gmail.com>
2023-08-16 08:53:02 +0000

    fixed pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,12 +9,10 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -22,8 +20,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -37,4 +34,3 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
